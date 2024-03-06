798c7aa5c7950f2119611515a19be6a541a046c8
Marcelo <139832086+Celim1@users.noreply.github.com>
2024-03-06 15:14:27 -0300

    Update primeiro.py

--- primeiro.py
+++ primeiro.py
@@ -1,28 +1,14 @@
 import pandas as pd
-import streamlit as st
-from requests import get
-url_api_bovespa = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.4334/dados?formato=json"
-def carregar_siglas():
-    """
-    Função para carregar as siglas da bolsa da Ibovespa
+tabelas = pd.read_html("https://infomoney.com.br/cotacoes/empresas-b3/")
 
-    Retorna:
-        DataFrame com as siglas e os nomes das empresas
-    """
+lista_empresas = []
+for tabela in tabelas:
+    lista_empresas.append(tabela.Ativos)
+
+lista_empresas = [tabela.Ativos for tabela in tabelas]
+empresas = []
+for item in lista_empresas:
+    empresas += list(item)
+
+len(empresas)
     
-    resposta = get(url_api_bovespa)
-    dados = resposta.json()
-    
-    df_siglas = pd.DataFrame(dados["series"][0]["dados"])
-    df_siglas.columns = ["data", "sigla", "valor"]
-    
-    return df_siglas[["sigla", "valor"]]
-
-df_siglas = carregar_siglas()
-
-st.title("Ibovespa - Selecione a Sigla")
-
-sigla_selecionada = st.checkbox("Mostrar valor", value=False)
-
-if sigla_selecionada:
-    st.write(df_siglas.to_html())
